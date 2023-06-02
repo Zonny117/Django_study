fbeb06ea65a9819a49538b373e67d6b87dc70e05
zonny117 <wonj1208@naver.com>
2023-06-02 17:53:59 +0900

    first-upload

--- instagram/content/models.py
+++ instagram/content/models.py
@@ -8,3 +8,7 @@ class Feed(models.Model):
     profile_image = models.TextField()  # 프로필 이미지
     user_id = models.TextField()  # 글쓴이
     like_count = models.IntegerField()  # 좋아요 수
+
+
+# python manage.py makemigrations 명령어를 통해 models.py에 작성된 클래스를 데이터베이스를 위한 모델로 생성한다.
+# python manage.py migrate 명령어로 생성된 모델을 실제 데이터베이스의 테이블로 작성한다. 이때 테이블명은 폴더명_클래스명이 된다.
--- instagram/content/views.py
+++ instagram/content/views.py
@@ -1,6 +1,10 @@
+from uuid import uuid4
 from django.shortcuts import render
+from rest_framework.views import Response
 from rest_framework.views import APIView
 from .models import Feed
+from instagram.settings import MEDIA_ROOT
+import os
 
 
 # Create your views here.
@@ -15,3 +19,33 @@ class Main(APIView):
             "instagram/main.html",
             context=dict(feeds=feed_list),
         )
+
+
+class UploadView(APIView):
+    def post(self, request):
+        # 파일 불러오기
+        file = request.FILES["file"]
+        # 파일명에 한글이나 특수문자 등을 랜덤한 문자로 처리함
+        uuid_name = uuid4().hex
+        # 저장 경로 설정 - MEDIA_ROOT는 설정 파일에서 BASE_DIR(프로젝트 경로)에 'media'를 붙인 변수이며, 여기에 uuid_name을 붙여서
+        # 최종적으로 프로젝트 경로/media/uuid_name의 경로로 파일이 저장될 것이다.
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+        # 실제 파일이 저장되는 코드, 파일을 열어서 파일 청크를 for문을 통해 하나하나 저장해나간다.
+        with open(save_path, "wb+") as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        image = uuid_name
+        content = request.data.getlist("file")[1]
+        user_id = request.data.getlist("file")[2]
+        profile_image = request.data.getlist("file")[3]
+
+        Feed.objects.create(
+            profile_image=profile_image,
+            user_id=user_id,
+            image=image,
+            content=content,
+            like_count=0,
+        )
+
+        return Response(status=200)
